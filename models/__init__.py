004d86579cc7b357763a5a54cb63a385765483aa
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-11-12 23:14:28 +0100

    delet thisngs

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Entry point script for your application"""
-from models.engine.file_storage import FileStorage
-
-# Create a unique FileStorage instance for your application
-storage = FileStorage()
-
-# Reload data into the storage instance
-storage.reload()
--- models/engine/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''Application FileStorage Configuration'''
-from models.engine.file_storage import FileStorage
-
-'''Create a unique FileStorage instance for the application'''
-storage = FileStorage()
-
-'''Reload data into the storage instance'''
-storage.reload()
