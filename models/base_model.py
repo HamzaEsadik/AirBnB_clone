9a77478d0d5bf3aeacfbba7dd08262d8f7b9c57e
Gnomedebian <yassineamgarou@gmail.com>
2023-11-12 21:46:46 +0100

    push checker

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,45 @@
-#!/usr/bin/env bash
-'''Base Model'''
+#!/usr/bin/python3
+'''module base_model'''
+
+
 import uuid
 from datetime import datetime
-class BaseModel:
-    '''Base model class'''
-    def __init__(self):
-        '''the init method'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+import models
+
+
+class BaseModel():
+    '''class BaseModel'''
+    def __init__(self, *args, **kwargs):
+        '''class constructor for class BaseModel'''
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        '''str method'''
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-    
+        '''string of BaseModel instance'''
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
     def save(self):
-        '''update update_at with current time'''
+        '''updates 'updated_at' instance with current datetime'''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-        new_dict = self.__dict__
-        new_dict["__class__"] = self.__class__.__name__
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        return new_dict
+        '''dictionary representation of instance'''
+        new_dict = dict(self.__dict__)
+        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return (new_dict)
